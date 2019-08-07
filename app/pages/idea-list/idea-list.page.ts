079294a03cb5c13490de3b226272bb00a880c484
souphan keodouangkham <souphan.dev@gmail.com>
2019-08-06 22:22:43 -0700

    Auto-generated commit

---
+++ app/pages/idea-list/idea-list.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { IdeaService, Idea } from 'src/app/services/idea.service';
+import { Observable } from 'rxjs';
+ 
+@Component({
+  selector: 'app-idea-list',
+  templateUrl: './idea-list.page.html',
+  styleUrls: ['./idea-list.page.scss'],
+})
+export class IdeaListPage implements OnInit {
+ 
+  professionals: Observable<any[]>;
+ 
+  constructor(private ideaService: IdeaService) { }
+ 
+  ngOnInit() {
+    this.professionals = this.ideaService.getProfessionals();
+  }
+}
---
+++ environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC63JUyBGNr5uxTsxyfa-WkX5jRgzGYKDk",
+    authDomain: "lao-professionals.firebaseapp.com",
+    databaseURL: "https://lao-professionals.firebaseio.com",
+    projectId: "lao-professionals",
+    storageBucket: "",
+    messagingSenderId: "144841870718",
+    appId: "1:144841870718:web:f2d1903d402ff587"
+  }
+};
