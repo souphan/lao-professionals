588d3fb3dab1458ea2464a4c228e404edb94d5be
souphan keodouangkham <souphan.dev@gmail.com>
2019-08-17 15:51:20 -0700

    adding image uploader

--- src/app/pages/idea-list/idea-list.page.ts
+++ src/app/pages/idea-list/idea-list.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IdeaService, Idea } from 'src/app/services/idea.service';
 import { Observable } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
+import { delay,tap, map, filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-idea-list',
@@ -18,5 +19,22 @@ export class IdeaListPage implements OnInit {
  
   ngOnInit() {
     this.professionals = this.ideaService.getProfessionals();
+  }
+
+  filterList(evt) {  
+    const searchTerm = evt.srcElement.value;
+  
+    if (!searchTerm) {
+      return;
+    }
+  
+    this.professionals = this.professionals.filter(pro => {
+      if (pro.goalName && searchTerm) {
+        if (pro.goalName.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+          return true;
+        }
+        return false;
+      }
+    });
   }
 }
